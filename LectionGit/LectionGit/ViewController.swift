a5a754be9a386f588a12da5d1b19dc5be6f1e282
Роман Родителев <>
2019-07-08 20:41:43 +0300

    Added firs project

---
+++ LectionGit/LectionGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LectionGit
+//
+//  Created by Роман Родителев on 7/8/19.
+//  Copyright © 2019 Роман Родителев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
